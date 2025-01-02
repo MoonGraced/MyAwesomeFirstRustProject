66ed1f4010f1f98a8e219be4d20edc526d40a46c
Кирилл Андреевич <talathel@vk.com>
2025-01-02 22:02:47 +0300

    Guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret: u32 = rand::random_range(1..=100);
+    // println!("Number {secret}");
+    loop {
+        let mut guess_number: String = String::new();
+        println!("Please input your guess!");
+        std::io::stdin()
+            .read_line(&mut guess_number)
+            .expect("Failed to read line");
+        let guess_number: u32 = match guess_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess_number.cmp(&secret) {
+            std::cmp::Ordering::Less => {
+                println!("{guess_number} is oo small!");
+            },
+            std::cmp::Ordering::Equal => {
+                println!("Yay! Equal!");
+                break;
+            },
+            std::cmp::Ordering::Greater => {
+                println!("Yikes! {guess_number} is too big!");
+            },
+        }
+    }
 }
